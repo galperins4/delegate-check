a34498c0016dcdfe218f80641930fc5756e10bda
galperins4 <galperins4@gmail.com>
2019-03-12 21:35:48 -0500

    Update config.py

--- config/config.py
+++ config/config.py
@@ -37,38 +37,38 @@ class Config():
 
 
     def load_api(self):
-        self.ark_api = os.getenv("ARK_API")
-        self.dark_api = os.getenv("DARK_API")
-        self.qredit_api = os.getenv("QREDIT_API")
-        self.phantom_api = os.getenv("PHANTOM_API")
-        self.persona_api = os.getenv("PERSONA_API")
-        self.ripa_api = os.getenv("RIPA_API")
-        self.swapblocks_api = os.getenv("SWAPBLOCKS_API")
-        self.blockpool_api = os.getenv("BLOCKPOOL_API")
+        ark_api = os.getenv("ARK_API")
+        dark_api = os.getenv("DARK_API")
+        qredit_api = os.getenv("QREDIT_API")
+        phantom_api = os.getenv("PHANTOM_API")
+        persona_api = os.getenv("PERSONA_API")
+        ripa_api = os.getenv("RIPA_API")
+        swapblocks_api = os.getenv("SWAPBLOCKS_API")
+        blockpool_api = os.getenv("BLOCKPOOL_API")
         api = [ark_api,dark_api,qredit_api,phantom_api,persona_api,ripa_api,swapblocks_api,blockpool_api]
         
         return api
 
     
     def load_network(self):
-        self.ark_net = os.getenv("ARK_NET").split(',')
-        self.dark_net = os.getenv("DARK_NET").split(',')
-        self.qredit_net = os.getenv("QREDIT_NET").split(',')
-        self.phantom_net = os.getenv("PHANTOM_NET").split(',')
-        self.persona_net = os.getenv("PERSONA_NET").split(',')
-        self.ripa_net = os.getenv("RIPA_NET").split(',')
-        self.swapblocks_net = os.getenv("SWAPBLOCKS_NET").split(',')
-        self.blockpool_net = os.getenv("BLOCKPOOL_NET").split(',')
+        ark_net = os.getenv("ARK_NET").split(',')
+        dark_net = os.getenv("DARK_NET").split(',')
+        qredit_net = os.getenv("QREDIT_NET").split(',')
+        phantom_net = os.getenv("PHANTOM_NET").split(',')
+        persona_net = os.getenv("PERSONA_NET").split(',')
+        ripa_net = os.getenv("RIPA_NET").split(',')
+        swapblocks_net = os.getenv("SWAPBLOCKS_NET").split(',')
+        blockpool_net = os.getenv("BLOCKPOOL_NET").split(',')
         network = [ark_net,dark_net,qredit_net,phantom_net,persona_net,ripa_net,swapblocks_net,blockpool_net]
     
         return network
     
     
     def load_epoch(self):
-        self.persona_epoch = os.getenv("PERSONA_EPOCH").split(',')
-        self.ripa_epoch = os.getenv("RIPA_EPOCH").split(',')
-        self.swapblocks_epoch = os.getenv("SWAPBLOCKS_EPOCH").split(',')
-        self.blockpool_epoch = os.getenv("BLOCKPOOL_EPOCH").split(',')
+        persona_epoch = os.getenv("PERSONA_EPOCH").split(',')
+        ripa_epoch = os.getenv("RIPA_EPOCH").split(',')
+        swapblocks_epoch = os.getenv("SWAPBLOCKS_EPOCH").split(',')
+        blockpool_epoch = os.getenv("BLOCKPOOL_EPOCH").split(',')
         epoch = [persona_epoch,ripa_epoch,swapblocks_epoch,blockpool_epoch]
         
         return epoch
