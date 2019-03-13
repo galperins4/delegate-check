fd201bba343c5bd27f15617f5145b4efa5976415
galperins4 <galperins4@gmail.com>
2019-03-12 21:15:45 -0500

    Update config.py

--- config/config.py
+++ config/config.py
@@ -7,3 +7,28 @@ class Config():
         self.home = str(Path.home())
         env_path = self.home + '/delegate-check/config/config'
         load_dotenv(env_path)
+        self.load_sns()
+        self.load_delegates()
+        self.load_api()
+        self.load_network()
+        self.load_epoch()
+        
+
+    def load_sns(self):
+        pass
+    
+    
+    def load_delegates(self):
+        pass
+
+
+    def load_api(self):
+        pass
+
+    
+    def load_network(self):
+        pass
+    
+    
+    def load_epoch(self):
+        pass
