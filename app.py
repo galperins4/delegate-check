eef26522fb7c00f2488e45728007364a8a544bd7
galperins4 <galperins4@gmail.com>
2019-03-12 22:03:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from chalice import Chalice
+#from chalice import Chalice
+#from util.async import Async
+from config.config import Config
 
-app = Chalice(app_name='delcheck')
 
+#app = Chalice(app_name='delcheck')
 
+'''
 @app.route('/')
 def index():
     delegate_check()
     return {'status': 'success'}
 
-
+'''
 def delegate_check():
     pass
+
+
+if __name__ == '__main__':
+    c = Config
+    print(c.delegates)
+    print(c.apis)
+    print(c.networks)
+    print(c.epochs)
+    print(c.sns_enabled)
+    print(c.aws_key_id)
+    print(c.aws_secret_key)
+    print(c.aws_region)
+    print(c.phone)
