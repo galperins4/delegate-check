3db3c8ef5b14805b00ee1db1250bbcf363d3df5e
galperins4 <galperins4@gmail.com>
2019-03-12 21:12:47 -0500

    Create config.py

---
+++ config/config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+class Config():
+    def __init__(self):
+        self.home = str(Path.home())
+        env_path = self.home + '/delegate-check/config/config'
+        load_dotenv(env_path)
