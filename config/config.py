bbdb060373d5539bb28cf76558d7ecdb9bd2880f
galperins4 <galperins4@gmail.com>
2019-03-12 23:06:21 -0500

    Update config.py

--- config/config.py
+++ config/config.py
@@ -95,5 +95,4 @@ class Config():
                                      
     
     def format_str(self,n):
-        return [int(n[0]), int(n[1]), int(n[2]), int(n[3]), int(n[4])
-                                     
+        return [int(n[0]), int(n[1]), int(n[2]), int(n[3]), int(n[4])]
